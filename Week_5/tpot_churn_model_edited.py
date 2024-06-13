7198fa40903bf17a93e36397a8edcdf6e2392555
prestond28 <142540606+prestond28@users.noreply.github.com>
2024-06-12 22:23:48 -0500

    Hopefully this works!

---
+++ Week_5/tpot_churn_model_edited.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.model_selection import train_test_split
+
+# NOTE: Make sure that the outcome column is labeled 'target' in the data file
+tpot_data = pd.read_csv('new_churn_data.csv', index_col="customerID")
+features = tpot_data.drop('churn', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['churn'], random_state=28)
+
+# Average CV score on the training set was: 0.7991288237149164
+exported_pipeline = ExtraTreesClassifier(bootstrap=True, criterion="entropy", max_features=0.8, min_samples_leaf=17, min_samples_split=9, n_estimators=100)
+# Fix random state in exported estimator
+if hasattr(exported_pipeline, 'random_state'):
+    setattr(exported_pipeline, 'random_state', 28)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
+print(results)
